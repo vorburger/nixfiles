1bb3f9dafa1f030a4c24f1fe17aee3d17e2f11e6
Michael Vorburger <mike@vorburger.ch>
2025-12-26 17:13:00 +0100

    Refactor: Move home-manager config for vorburger to user module

--- modules/hosts/test1.nix
+++ modules/hosts/test1.nix
@@ -2,18 +2,15 @@
 {
   flake.nixosConfigurations.test1 = inputs.nixpkgs.lib.nixosSystem {
     system = "x86_64-linux";
+    specialArgs = { inherit inputs; };
     modules = [
       inputs.disko.nixosModules.disko
-      inputs.home-manager.nixosModules.home-manager
       ../disko/_boot-and-ext4.nix
       ../services/_networking.nix
       ../services/_openssh.nix
       ../users/_tester.nix
       ../users/_vorburger.nix
       {
-        # TODO How can this be moved into vorburger.nix ?!
-        home-manager.users.vorburger = import "${inputs.vorburger-dotfiles}/home.nix";
-
         boot.loader.grub.enable = true;
         boot.loader.grub.devices = [ "/dev/sda" ]; # TODO use disko to get device
 
