ec6602a0e7fada4c7a54c4b30070b733937f974b
Michael Vorburger <mike@vorburger.ch>
2025-12-26 16:47:17 +0100

    Fix config.fish clobber + make home-manager await network-online

--- modules/hosts/test1.nix
+++ modules/hosts/test1.nix
@@ -6,8 +6,18 @@
       inputs.disko.nixosModules.disko
       inputs.home-manager.nixosModules.home-manager
       {
+        # Required to avoid: "Existing file '/home/vorburger/.config/fish/config.fish' would be clobbered"
+        home-manager.backupFileExtension = "home-manager_backup";
+
+        systemd.services.home-manager-vorburger = {
+          after = [ "network-online.target" ];
+          wants = [ "network-online.target" ];
+        };
+
         home-manager.useGlobalPkgs = true;
         home-manager.useUserPackages = true;
+
+        # See https://github.com/vorburger/vorburger-dotfiles-bin-etc/blob/main/dotfiles/home-manager/flake.nix
         home-manager.extraSpecialArgs = {
           envHOME = "";
         };
@@ -22,6 +32,8 @@
         boot.loader.grub.devices = [ "/dev/sda" ]; # TODO use disko to get device
 
         networking.hostName = "test1";
+        networking.networkmanager.enable = true;
+        systemd.services.NetworkManager-wait-online.enable = true;
 
         # virtualisation.forwardPorts = [
         #   {
