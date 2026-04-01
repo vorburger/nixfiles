fd10bfcd87110ad7acc4fd2146ef4bfc96658647
Michael Vorburger <mike@vorburger.ch>
2026-04-01 22:29:15 +0200

    security: PermitRootLogin no instead prohibit-password

--- modules/services/_openssh.nix
+++ modules/services/_openssh.nix
@@ -1,6 +1,6 @@
 {
   services.openssh.enable = true;
-  services.openssh.settings.PermitRootLogin = "prohibit-password";
+  services.openssh.settings.PermitRootLogin = "no";
   services.openssh.settings.PasswordAuthentication = false;
   services.openssh.settings.AllowTcpForwarding = false;
   services.openssh.settings.X11Forwarding = false;
