c102d0ec2c5c7b0327fa0ce6cf9c5b459b6da0fd
Michael Vorburger <mike@vorburger.ch>
2026-05-14 18:01:31 +0200

    ui: GNOME minimal

--- modules/services/_gnome.nix
+++ modules/services/_gnome.nix
@@ -1,6 +1,30 @@
+{ pkgs, ... }:
+
 {
   services = {
     displayManager.gdm.enable = true;
     desktopManager.gnome.enable = true;
   };
+
+  environment.gnome.excludePackages = with pkgs; [
+    atomix # puzzle game
+    cheese # webcam tool
+    epiphany # web browser
+    evince # document viewer
+    geary # email reader
+    gedit # text editor
+    gnome-calendar
+    gnome-characters
+    gnome-contacts
+    gnome-maps
+    gnome-music
+    gnome-photos
+    gnome-terminal
+    gnome-tour
+    gnome-weather
+    hitori # sudoku game
+    iagno # go game
+    tali # poker game
+    totem # video player
+  ];
 }
