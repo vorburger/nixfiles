f462dcbf418c4b20e6c7b2614738ea482b7e556b
Michael Vorburger <mike@vorburger.ch>
2026-05-14 13:32:35 +0200

    nix run .#write-flake

--- update.sh
+++ update.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+# DEPRECATED!! Instead, prefer updating Flake inputs using:
+#   - .agents/skills/nix-update skill on your local machine, and
+#   - dependabot auto-merge workflow on GitHub
+
 set -euo pipefail
 
 nix flake update
