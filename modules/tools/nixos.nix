277d7ff208cea6d3156f7aa74338df8ef2f78d04
Michael Vorburger <mike@vorburger.ch>
2026-05-30 16:04:47 +0200

    nix-fast-build

--- modules/tools/nixos.nix
+++ modules/tools/nixos.nix
@@ -4,6 +4,7 @@
     {
       devshells.default = {
         devshell.packages = [
+          pkgs.nix-fast-build
           pkgs.nixos-anywhere
           pkgs.nixos-install-tools
           pkgs.nixos-rebuild
