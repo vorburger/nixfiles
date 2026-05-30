5360228d538c795eeb9b9157bf08b9a49ec68c03
Michael Vorburger <mike@vorburger.ch>
2026-05-31 00:05:04 +0200

    nh

--- modules/tools/nixos.nix
+++ modules/tools/nixos.nix
@@ -4,6 +4,7 @@
     {
       devshells.default = {
         devshell.packages = [
+          pkgs.nh # https://github.com/nix-community/nh
           pkgs.nix-output-monitor
           pkgs.nix-fast-build
           pkgs.nixos-anywhere
