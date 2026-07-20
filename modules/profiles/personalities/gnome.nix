94d54397364d71a91235da4fecfd617b31a4ff4b
Michael Vorburger <mike@vorburger.ch>
2026-07-20 23:48:47 +0200

    Configure GST_PLUGIN_SYSTEM_PATH_1_0 in GNOME personality to enable audio/video file properties in Nautilus

--- modules/profiles/personalities/gnome.nix
+++ modules/profiles/personalities/gnome.nix
@@ -1,9 +1,26 @@
 {
   flake.nixosModules.personality-gnome =
-    { self, ... }:
+    {
+      pkgs,
+      lib,
+      self,
+      ...
+    }:
     {
       imports = [ self.nixosModules.ui ];
 
       services.gnome-extra.enable = true;
+
+      environment.sessionVariables.GST_PLUGIN_SYSTEM_PATH_1_0 =
+        lib.makeSearchPathOutput "lib" "lib/gstreamer-1.0"
+          (
+            with pkgs.gst_all_1;
+            [
+              gst-plugins-good
+              gst-plugins-bad
+              gst-plugins-ugly
+              gst-libav
+            ]
+          );
     };
 }
