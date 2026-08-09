c52ad76f9529da2c07e1cd1914a0fdf9a64a14cc
Michael Vorburger <mike@vorburger.ch>
2026-08-09 02:28:55 +0200

    fprintd: maxTries 10

--- docs/docs/reference/fprintd.md
+++ docs/docs/reference/fprintd.md
@@ -8,3 +8,13 @@
 Beware that just `sudo fprintd-enroll` confusingly also does work,
 but that sets the fingerprints of the `root` user, which is not
 what you'll want.
+
+## Configuration
+
+Enable extra fprintd configuration with `services.fprintd-extra.enable = true;`.
+
+You can adjust the allowed fingerprint authentication attempts before falling back to password input using:
+
+```nix
+services.fprintd-extra.maxTries = 10; # Defaults to 10 attempts
+```
--- modules/services/fprintd-extra.nix
+++ modules/services/fprintd-extra.nix
@@ -9,11 +9,25 @@ in
   flake.nixosModules.fprintd-extra = mkService {
     name = "fprintd-extra";
     description = "extra fprintd configuration";
-    content = {
-      services.fprintd.enable = true;
-      # Remember to enroll fingerprints with `fprintd-enroll` (for each user).
-      security.pam.services.login.fprintAuth = lib.mkDefault true;
-      security.pam.services.sudo.fprintAuth = true;
+    extraOptions = {
+      maxTries = lib.mkOption {
+        type = lib.types.int;
+        default = 10;
+        description = "Allowed attempts for fingerprint authentication in PAM before falling back to password.";
+      };
     };
+    content =
+      { cfg, ... }:
+      {
+        services.fprintd.enable = true;
+        # Remember to enroll fingerprints with `fprintd-enroll` (for each user).
+        security.pam.services.login.fprintAuth = lib.mkDefault true;
+        security.pam.services.sudo.fprintAuth = true;
+
+        security.pam.services.sudo.rules.auth.fprintd.settings.max-tries = cfg.maxTries;
+        security.pam.services.gdm-fingerprint.rules.auth.fprintd.settings.max-tries = cfg.maxTries;
+        security.pam.services.login.rules.auth.fprintd.settings.max-tries = cfg.maxTries;
+        security.pam.services.polkit-1.rules.auth.fprintd.settings.max-tries = cfg.maxTries;
+      };
   };
 }
