597a4490ea0e412d2b9375843707bcc975715b48
Michael Vorburger <mike@vorburger.ch>
2026-08-09 23:00:42 +0200

    http: vorbflix

--- modules/services/caddy.nix
+++ modules/services/caddy.nix
@@ -14,33 +14,37 @@ in
         virtualHosts."http://hello.home.vorburger.ch".extraConfig = ''
           respond "hello, caddy!"
         '';
-        # TODO vorbflix.home.vorburger.ch reverse_proxy :8096
+        virtualHosts."http://vorbflix.home.vorburger.ch".extraConfig = ''
+          reverse_proxy :8096
+        '';
         # TODO Does Jellyfin Chromecast work with reverse_proxy? Does it need TLS?
+        # TODO Auth! WebAuthn, ideally... check github.com/greenpau/caddy-security, or Authelia or Authentik.
         # TODO Services overview welcome sort of page; static, or auto-generated?
         # TODO http://localhost/ should show ^^^ it
         # TODO http://home.vorburger.ch/ should show ^^^ it
         # TODO http://titan.home.vorburger.ch/ should show ^^^ it
       };
 
+      # DNS
       services.coredns = {
         enable = true;
         config = ''
           . {
             hosts {
-              # TODO Use "IP of host", instead of hard-coding...
-              192.168.1.99 hello.home.vorburger.ch
+              # TODO Automagically add all virtualHosts!
+              # TODO Use "IP of host", instead of hard-coding 192.168.1.99...
+              192.168.1.99 hello.home.vorburger.ch vorbflix.home.vorburger.ch
               fallthrough
             }
-            # TODO Use "IP of network's default DNS", instead of hard-coding...
+            # TODO Use "IP of network's default DNS", instead of hard-coding 192.168.1.1...
             forward . 192.168.1.1 # Forward everything else to upstream
           }
         '';
       };
-
       # Use our very own DNS server (CoreDNS, above) for all lookups,
       # so that we can resolve our own local hostnames (like hello.home.vorburger.ch).
       # Alternatively, configure this host's IP on the home router's DHCP DNS setting.
-      # This works for the networkmanager from networking-extra.nix, but needs to be changed if we're ever switching to systemd-resolved or so.
+      # This works for networkmanager from networking-extra.nix, but needs to be changed if we're ever switching to systemd-resolved or other.
       networking.nameservers = [ "127.0.0.1" ];
     };
   };
