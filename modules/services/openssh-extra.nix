8a2c47c2a3e409aa5f0dc359b2b5eb87f3472e1f
Michael Vorburger <mike@vorburger.ch>
2026-08-09 17:56:07 +0200

    security: openssh KbdInteractiveAuthentication = false

--- modules/services/openssh-extra.nix
+++ modules/services/openssh-extra.nix
@@ -9,6 +9,7 @@ in
       services.openssh.enable = true;
       services.openssh.settings.PermitRootLogin = "no";
       services.openssh.settings.PasswordAuthentication = false;
+      services.openssh.settings.KbdInteractiveAuthentication = false;
       services.openssh.settings.AllowTcpForwarding = false;
       services.openssh.settings.X11Forwarding = false;
 
