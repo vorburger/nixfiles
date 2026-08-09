d3af8fe479d836e25a39fb591ee7ca8ec8ce31ec
Michael Vorburger <mike@vorburger.ch>
2026-08-09 22:21:50 +0200

    http: Caddy non-SSL http://hello.home.vorburger.ch/ OK (but TLS Cert ToDo)

--- modules/services/caddy.nix
+++ modules/services/caddy.nix
@@ -10,14 +10,38 @@ in
       services.caddy = {
         enable = true;
         openFirewall = false; # TODO reverse_proxy for all services, so that we can open the firewall for Caddy and close for all the other HTTP services
-        virtualHosts."localhost".extraConfig = ''
+        # TODO Remove http: (default is https:) once we have magic TLS working...
+        virtualHosts."http://hello.home.vorburger.ch".extraConfig = ''
           respond "hello, caddy!"
         '';
-        # TODO hello.home.vorburger.ch, with DNS
-        # TODO reverse_proxy :
-        # TODO https://wiki.nixos.org/wiki/Caddy "tls internal" - needed by Chromecast?
+        # TODO vorbflix.home.vorburger.ch reverse_proxy :8096
+        # TODO Does Jellyfin Chromecast work with reverse_proxy? Does it need TLS?
         # TODO Services overview welcome sort of page; static, or auto-generated?
+        # TODO http://localhost/ should show ^^^ it
+        # TODO http://home.vorburger.ch/ should show ^^^ it
+        # TODO http://titan.home.vorburger.ch/ should show ^^^ it
       };
+
+      services.coredns = {
+        enable = true;
+        config = ''
+          . {
+            hosts {
+              # TODO Use "IP of host", instead of hard-coding...
+              192.168.1.99 hello.home.vorburger.ch
+              fallthrough
+            }
+            # TODO Use "IP of network's default DNS", instead of hard-coding...
+            forward . 192.168.1.1 # Forward everything else to upstream
+          }
+        '';
+      };
+
+      # Use our very own DNS server (CoreDNS, above) for all lookups,
+      # so that we can resolve our own local hostnames (like hello.home.vorburger.ch).
+      # Alternatively, configure this host's IP on the home router's DHCP DNS setting.
+      # This works for the networkmanager from networking-extra.nix, but needs to be changed if we're ever switching to systemd-resolved or so.
+      networking.nameservers = [ "127.0.0.1" ];
     };
   };
 }
--- modules/services/networking-extra.nix
+++ modules/services/networking-extra.nix
@@ -8,6 +8,7 @@ in
     content = {
       # TODO Laptops: networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
+      # NB: If ever switching from NetworkManager to systemd-resolved, then see nameservers in caddy.nix
       networking.networkmanager.enable = true;
       systemd.services.NetworkManager-wait-online.enable = true;
 
